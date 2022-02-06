13a00a5019ebb4039813a9a68dc5242c826c3805
Jacky Romano <jacky.romano@intel.com>
2022-02-06 22:43:02 +0200

    Use batch-mode for build

--- tools/build_gazelle_jni.sh
+++ tools/build_gazelle_jni.sh
@@ -8,6 +8,7 @@ do_clean=""
 BUILD_ARROW=ON
 CLEAN_CPP=ON
 DO_CLEAN=clean
+BATCH_MODE=
 
 for arg in "$@"
 do
@@ -16,6 +17,10 @@ do
             DO_CLEAN=
             BUILD_ARROW=OFF
             CLEAN_CPP=OFF
+            shift
+            ;;
+        -b|--batch)
+            BATCH_MODE="-B"
             shift
             ;;
         -h|--help)
@@ -29,5 +34,5 @@ done
 
 
 pushd $root
-mvn $DO_CLEAN package -P full-scala-compiler -Dbuild_arrow=${BUILD_ARROW} -Dbuild_cpp=ON -Dclean_cpp=${CLEAN_CPP} -DskipTests -Dcheckstyle.skip
+mvn $DO_CLEAN package $BATCH_MODE -P full-scala-compiler -Dbuild_arrow=${BUILD_ARROW} -Dbuild_cpp=ON -Dclean_cpp=${CLEAN_CPP} -DskipTests -Dcheckstyle.skip
 popd 
