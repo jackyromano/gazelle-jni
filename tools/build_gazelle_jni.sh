fb7f68891964f5465de433dbbdf013c4cbf34571
Jacky Romano <jacky.romano@intel.com>
2022-03-22 12:37:26 +0200

    Add more artifacts

--- tools/build_gazelle_jni.sh
+++ tools/build_gazelle_jni.sh
@@ -48,5 +48,5 @@ cp -r tests $artifacts_dir
 cp xiphos-spark-integration/resources/config.yaml $artifacts_dir
 cp xiphos-spark-integration/xiphos-tools/build/table_info/table_info $artifacts_dir
 cp xiphos-spark-integration/xiphos-tools/build/data_ingestion/parquet/xiphos-parquet-loader $artifacts_dir   
-cp cpp/build/releases/libspark_columnar_jni.so $artifacts_dir
+rsync -Pa cpp/build/releases/* $artifacts_dir/
 popd 
