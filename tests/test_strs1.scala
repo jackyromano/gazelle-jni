0e0490d850946fd3505ed17ecd3a468b71d31d70
Jacky Romano <jacky.romano@intel.com>
2022-04-17 08:56:18 +0000

    Temporaty add strs1 tests

---
+++ tests/test_strs1.scala
@@ -0,0 +1,9 @@
+spark.sparkContext.setLogLevel("DEBUG")
+val strings_df = spark.read.format("com.intel.dbio.sources.datasourcev2.xiphosv2").load("strs1")
+strings_df.createTempView("test_strings")
+val query_df = spark.sql("select * from test_strings")
+query_df.explain
+query_df.show
+:quit
+
+
