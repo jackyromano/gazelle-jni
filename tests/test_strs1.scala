e59970ef0a101241464c91e26577ddacaadff50d
Jacky Romano <jacky.romano@intel.com>
2022-04-17 12:27:40 +0000

    STRS1 tests

--- tests/test_strs1.scala
+++ tests/test_strs1.scala
@@ -1,4 +1,4 @@
-spark.sparkContext.setLogLevel("DEBUG")
+//spark.sparkContext.setLogLevel("DEBUG")
 val strings_df = spark.read.format("com.intel.dbio.sources.datasourcev2.xiphosv2").load("strs1")
 strings_df.createTempView("test_strings")
 val query_df = spark.sql("select * from test_strings")
