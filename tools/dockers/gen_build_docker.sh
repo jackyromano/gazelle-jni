5ca2611b4dbae7d1ec58608a8cb4043c4f736bad
Jacky Romano <jacky.romano@intel.com>
2022-04-17 08:12:19 +0000

    Update NB-DAXL version + docker fixes

--- tools/dockers/gen_build_docker.sh
+++ tools/dockers/gen_build_docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 mydir=$(realpath $(dirname $0))
 
-docker build -t dbio-dev-build1.iil.intel.com:5000/gazelle-jni-ci-image:latest $mydir/build_docker
-docker push dbio-dev-build1.iil.intel.com:5000/gazelle-jni-ci-image:latest
+docker_name=gazelle-jni-ci-image
+if [ "$#" -eq "1" ]; then
+    echo "set docker name to $1"
+    docker_name=$1
+fi
+docker build -t dbio-dev-build1.iil.intel.com:5000/${docker_name}:latest $mydir/build_docker
+docker push dbio-dev-build1.iil.intel.com:5000/${docker_name}:latest
 
